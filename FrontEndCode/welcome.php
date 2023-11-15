c1b6560087b64ac59256ca8beb9b52328f4a9930
diocletian53 <142997289+diocletian53@users.noreply.github.com>
2023-11-15 15:59:09 -0500

    Add files via upload

---
+++ FrontEndCode/welcome.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Jeopardy!</title>
+<link rel="stylesheet" href="style.css"> 
+</head>
+<body>
+
+<h1> Let's play Jeopardy!</h1>
+<br><br>
+<a href="index2.php"><button class="button"><p>Start a new game</p></button></a>
+<a href="leaderboard.php"><button class="button"><p>View leaderboards</p></button></a>
+  
+
+
+
+</body>
+</html>
