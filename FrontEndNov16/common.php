cce81812b0737b0ce6dfa773c915168b922b45bb
diocletian53 <142997289+diocletian53@users.noreply.github.com>
2023-11-16 14:15:56 -0500

    Add files via upload

---
+++ FrontEndNov16/common.php
@@ -0,0 +1,19 @@
+<?php
+
+function saveUser($uName, $pWord){
+  if(isset($uName) && isset($pWord)){
+			$inString = "\r\n".$uName.",". $pWord;
+			$fp = fopen('registerinfo.txt', 'a');
+			fwrite($fp, $inString);
+      fclose($fp);
+		}
+}
+
+function readLeaderBoard(){
+	$lboard = explode(";", file_get_contents("leaderboard.txt"));
+	echo"<p>LeaderBoard</p><hr>";
+	foreach($lboard as $board){
+	  echo "</br>".$board;      
+	}
+  }
+  ?>
---
+++ FrontEndNov16/leaderboard.php
@@ -0,0 +1,21 @@
+<?php 
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Jeopardy!</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Allerta+Stencil">
+</head>
+<body>
+ <div class = "leaderboard">
+    <?php
+    include "common.php";
+    readLeaderBoard(); 
+    ?>
+ </div>
+ <button><a href="index.php">Main Menu</a></button>
+  </body>
+</html>
